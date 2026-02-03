71e384a995bd6d87b5c0196dccf464a02f82413f
tech <154914363+techplayz32@users.noreply.github.com>
2026-02-03 18:41:11 +0300

    Update IP addresses for IPv4 and IPv6

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -28,8 +28,8 @@ struct BuiltInDc {
 	int port;
 };
 
-#define IpV4 "192.168.1.100"
-#define IpV6 "192.168.1.100"
+#define IpV4 "192.168.3.215"
+#define IpV6 "192.168.3.215"
 #define IpV4Port 20443
 #define IpV4Port2 20443
 #define IpV6Port 20443
